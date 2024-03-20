e367261ac9f7285a8383d4fdc172ceea74eef38f
Zephania <zephania1656@live.com>
2024-03-20 20:13:29 +0000

    create structure for dash

--- Mocktologist/pages/dash.js
+++ Mocktologist/pages/dash.js
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react'
-import { View, Text, ImageBackground, TouchableOpacity } from "react-native";
+import { useState } from 'react'
+import { View, Text, ImageBackground, TouchableOpacity, Image, TouchableHighlight } from "react-native";
 import { useAuth } from '../hooks/useAuth'
 import { useOverlayPopup } from '../hooks/useOverlayPopup';
 import styles from '../style'
 
 export default function Dash() {
+    const [active, setActive] = useState(false)
     const { firstName } = useAuth();
 
     const { showOverlay, setShowOverlay, showPopup, setShowPopup } = useOverlayPopup();
@@ -30,14 +31,52 @@ export default function Dash() {
         )
     }
 
+    if (active) {
+        return (
+            <ImageBackground source={require("../assets/background.png")} style={styles.background}>
+                <View style={styles.container2}>
+                    {showOverlay && <Overlay />}
+                    {showPopup && <Popup />}
+                    <View style={styles.headingContainer}>
+                        <Text style={styles.heading}> How's it going, {firstName}? </Text>
+                    </View>
+                    <View style={styles.boxCont}>
+                        <View style={styles.dashBox}>
+
+                        </View>
+                        <View style={styles.dashBox}>
+
+                        </View>
+                    </View>
+                </View>
+            </ImageBackground>
+        )
+    }
+
     return (
         <ImageBackground source={require("../assets/background.png")} style={styles.background}>
-            <View style={styles.container}>
+            <View style={styles.container2}>
                 {showOverlay && <Overlay />}
                 {showPopup && <Popup />}
                 <View style={styles.headingContainer}>
                     <Text style={styles.heading}> How's it going, {firstName}? </Text>
                 </View>
+                <View style={styles.boxCont}>
+                    <View style={styles.dashBox}>
+
+                    </View>
+                    <View style={styles.dashBox}>
+
+                    </View>
+                </View>
+                <Text style={styles.dashText}> Ready to make something new? </Text>
+                <TouchableHighlight style={styles.button} underlayColor="#ED91C8">
+                    <Text style={styles.buttonText}> + New Mocktail </Text>
+                </TouchableHighlight>
+                <Image
+                    style={styles.bartender}
+                    source={require('../assets/bartender.png')}
+                />
             </View>
         </ImageBackground>
     );
